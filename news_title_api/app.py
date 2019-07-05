6cd046d12b4c4423031254b83e04b0dd8b0bf1ff
Dalya's Mac <hyansky@daum.net>
2019-07-05 19:13:04 +0900

    home

--- news_title_api/app.py
+++ news_title_api/app.py
@@ -5,6 +5,11 @@ from news_catch import news_title_catch
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+    return '환영합니다, /daum-news 로 이동해보세요!'
+
+
 @app.route('/daum-news', methods=['GET'])
 def duam_news_main():
     return '/daum-news/카테고리명 (사회, 정치, 경제, 문화, 국제, it)'
@@ -14,3 +19,6 @@ def duam_news_main():
 def daum_news(category):
     title  = news_title_catch(category)
     return jsonify(title)
+
+
+app.run(host='0.0.0.0')
