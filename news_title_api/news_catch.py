5dd82134da888d6050294a99374711231ecc0f8d
Dalya's Mac <hyansky@daum.net>
2019-07-25 00:12:47 +0900

    create md file

--- news_title_api/news_catch.py
+++ news_title_api/news_catch.py
@@ -1,14 +1,3 @@
-# 도움 받은 사이트
-# 크롤링 : https://medium.com/@mjhans83/%ED%8C%8C%EC%9D%B4%EC%8D%AC%EC%9C%BC%EB%A1%9C-%ED%81%AC%EB%A1%A4%EB%A7%81-%ED%95%98%EA%B8%B0-908e78ee09e0
-# 크롤링 : https://beomi.github.io/2017/01/20/HowToMakeWebCrawler/
-# 파이썬 전반 : https://dojang.io/ (파이썬 코딩 도장)
-# OrderedDict 관련: https://godoftyping.wordpress.com/2015/05/13/python-%EC%88%9C%EC%84%9C%EB%A5%BC-%EA%B8%B0%EC%96%B5%ED%95%98%EB%8A%94-%EC%82%AC%EC%A0%84%ED%98%95-ordereddict/
-# enum :
-# https://docs.python.org/3.4/library/enum.html
-# https://pythonkim.tistory.com/90
-# https://stackoverflow.com/questions/12801912/python-3-x-java-valueof-equivalent-in-python-3-x
-# 예외처리 : https://wayhome25.github.io/python/2017/02/26/py-12-exception/
-
 import requests, json
 from bs4 import BeautifulSoup
 from enum import Enum
