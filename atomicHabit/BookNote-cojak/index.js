818741d7ffdca3787d6bfc86887eb6dd4d0a18a9
Dal-ya <hyansky@daum.net>
2019-09-18 00:58:28 +0900

    view

--- atomicHabit/BookNote-cojak/index.js
+++ atomicHabit/BookNote-cojak/index.js
@@ -15,6 +15,8 @@ function writeBookNote(event) {
   const bookStatus = document.getElementById('book_status');
   const bookMemo = document.getElementById('book_memo');
   const bookDate = document.getElementById('book_date');
+  
+  // bookStatus 의 value가 아닌 text 가져오기.
   const bookStatusOptionsIndex = bookStatus.options.selectedIndex;
   const bookStatusText = bookStatus.options[bookStatusOptionsIndex].text;
   
@@ -25,7 +27,7 @@ function writeBookNote(event) {
       const bookNoteValueObj = {
         id: bookNoteStorageArr.length + 1,
         bookNameValue: bookName.value,
-        bookStatusTextValue: bookStatusText,
+        bookStatusValue: bookStatusText,
         bookMemoValue: bookMemo.value,
         bookDateValue: bookDate.value
       }
@@ -37,17 +39,36 @@ function writeBookNote(event) {
 
 
 function viewBookNote() {
-  const spanID = document.createElement('span');
-  const spanBookName = document.createElement('span');
-  const spanBookStatus = document.createElement('span');
-  const spanBookMemo = document.createElement('span');
-  const spanBookDate = document.createElement('span');
+  console.log(bookNoteStorageArr);
   
-  let []
+  bookNoteStorageArr.forEach(list => {
+    const divRow = document.createElement('div');
+    const spanID = document.createElement('span');
+    const spanBookName = document.createElement('span');
+    const spanBookStatus = document.createElement('span');
+    const spanBookMemo = document.createElement('span');
+    const spanBookDate = document.createElement('span');
 
-  spanID.className = 'cell col1';
-  spanID.innerText = 'test';
-  document.body.appendChild(spanID);
+    divRow.className = 'row';
+    spanID.className = 'cell col1';
+    spanBookName.className = 'cell col2';
+    spanBookStatus.className = 'cell col3';
+    spanBookMemo.className = 'cell col4';
+    spanBookDate.className = 'cell col5';
+
+    spanID.innerText = list.id;
+    spanBookName.innerText = list.bookNameValue;
+    spanBookStatus.innerText = list.bookStatusValue;
+    spanBookMemo.innerText = list.bookMemoValue;
+    spanBookDate.innerText = list.bookDateValue;
+
+    bookList.appendChild(divRow);
+    divRow.appendChild(spanID);
+    divRow.appendChild(spanBookName);
+    divRow.appendChild(spanBookStatus);
+    divRow.appendChild(spanBookMemo);
+    divRow.appendChild(spanBookDate);
+  });
 }
 
 
@@ -56,14 +77,17 @@ function getBookNote() {
   if(getBookNote !== null) {
     const parseGetBookNote  = JSON.parse(getBookNote);
     bookNoteStorageArr = parseGetBookNote;
+    viewBookNote();
   }
 }
 
+function delList() {
+
+}
 
 function init() {
   getBookNote();
   bookForm.addEventListener('submit', writeBookNote);
-  viewBookNote();
 }
 
 
